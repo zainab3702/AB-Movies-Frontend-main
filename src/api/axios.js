a803b886aae67990c35e2bdc98b4ec6d53629f82
zainab3702 <zainabfaiqua03@gmail.com>
2024-04-25 00:10:17 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://ab-movies-backend-7r0c.onrender.com";
 export default axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" }
